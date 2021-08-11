e4ba988ecc99b323d47ed128d8cd1a28253746b2
Masthan_Shaik <masthanmvb244@gmail.com>
2021-08-11 16:44:49 +0530

    added document to mongo altas

--- controllers/quote.js
+++ controllers/quote.js
@@ -2,6 +2,12 @@ const Quote = require('../models/quote');
 
 exports.getQuote = (request , response) => {
     Quote.countDocuments('quoteDesc', function(err, count){
+        if (err) {
+            response.send({
+                message: "Something went wrong."
+            })
+        } 
+
         const randomId = Math.floor( Math.random() * count);
 
         Quote.findOne({_id: randomId}, {quote: 1, by: 1, _id: 0},function(err, quote){
